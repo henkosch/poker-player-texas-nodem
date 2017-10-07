f786ae2755a3eb89582b60eba7141455314ffe46
Henrik <henrik.schnell@prosupport.io>
2017-10-07 10:30:13 +0200

    refactor

--- Player.js
+++ Player.js
@@ -9,12 +9,15 @@ class Player {
     try {
       const me = me(gameState);
       const cards = me.hole_cards;
-      if (onlyFigures(cards)) { 
+      if (onlyFigures(cards)) {
+        console.log("round ", gameState.round, " : ", "onlyFigures");
         bet(5000);
       } else {
+        console.log("round ", gameState.round, " : ", "NOT onlyFigures");
         bet(0);
       }
     } catch (e) {
+      console.log("round ", gameState.round, " : ", "ERROR", e);
       bet(5000);
     }
   }
@@ -29,6 +32,9 @@ function me(gameState) {
   return _.find(gameState.players, player => player.name == 'Texas Nodem');
 }
 
+const ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "A", "Q", "K", "J"]
+const figures = ["A", "Q", "K", "J"];
+
 function onlyFigures(cards) {
-  return _.every(cards, card => isNaN(parseInt(card.rank)) || card.rank == "10");
+  return _.every(cards, card => ranks.indexOf(card.rank) > 7);
 }
