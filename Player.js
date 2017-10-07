80578899427040f3f2191d42a0e8b79a79188751
Henrik <henrik.schnell@prosupport.io>
2017-10-07 10:37:10 +0200

    fix

--- Player.js
+++ Player.js
@@ -1,27 +1,28 @@
 const _ = require('lodash');
 
-class Player {
-  static ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "A", "Q", "K", "J"];
+function findMe(gameState) {
+  return _.find(gameState.players, player => player.name == 'Texas Nodem');
+}
 
+const ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "A", "Q", "K", "J"]
+const figures = ["A", "Q", "K", "J"];
+
+function onlyFigures(cards) {
+  return _.every(cards, card => ranks.indexOf(card.rank) > 7);
+}
+
+class Player {
   static get VERSION() {
     return '0.1';
   }
 
-  static me(gameState) {
-    return _.find(gameState.players, player => player.name == 'Texas Nodem');
-  }
-
-  static onlyFigures(cards) {
-    return _.every(cards, card => Player.ranks.indexOf(card.rank) > 7);
-  }
-
   static betRequest(gameState, bet) {
-    console.log(gameState);
+    console.log(gameState)
     try {
-      const me = Player.me(gameState);
+      const me = findMe(gameState);
       const cards = me.hole_cards;
       
-      if (Player.onlyFigures(cards)) {
+      if (onlyFigures(cards)) {
         console.log("round ", gameState.round, " : ", "onlyFigures");
         bet(5000);
       } else {
